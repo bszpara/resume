1ba9f966328fbce33357d444c7f47e1fe44055bb
bbsins <bbsins@localhost.localdomain>
2020-06-02 22:34:32 +0200

    First html version

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
  
-(function(){
-    console.log('invoking javascript')
-})();
+// just a stub for now , todo : print only printable  fragment
+// after safe consent template add to DOM
+
+    document.getElementById('saveAsPdf').onclick = function printPdf(){
+        window.print()
+    }
